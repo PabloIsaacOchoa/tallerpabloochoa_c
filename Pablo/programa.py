e4b022bf45d801aaecbfe614ed975e9306c7b849
PabloIsaacOchoa <136029286+PabloIsaacOchoa@users.noreply.github.com>
2023-06-16 01:47:20 +0000

    implementacion de modulos

--- Pablo/programa.py
+++
@@ -1,8 +0,0 @@
-from matematicas import sumar, restar, multiplicar, dividir
-from geometria import distancia
-
-print("Suma: ",sumar(2,3))
-print("Resta: ",restar(4,2))
-print("Multiplicar: ",multiplicar(3,3))
-print("Dividir: ",dividir(10,2))
-print("Distance: ",distancia(1,4,2,3))
